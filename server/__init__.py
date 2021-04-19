41ca9e92d0be8cccc800bb1b50bb638fcb4f904d
C.J. May <doctormay6@gmail.com>
2021-04-19 14:11:11 -0500

    added db aliases for documents

--- server/__init__.py
+++ server/__init__.py
@@ -5,7 +5,13 @@ from mongoengine import connect
 app = Flask(__name__)
 app.config.from_object(Config)
 
-connect(db=Config.DB_NAME,
+connect(db="agents",
+        alias="agent_db",
+        username=Config.DB_USER,
+        password=Config.DB_PASS,
+        host=Config.DB_URI)
+connect(db="admins",
+        alias="admin_db",
         username=Config.DB_USER,
         password=Config.DB_PASS,
         host=Config.DB_URI)
--- server/models.py
+++ server/models.py
@@ -1,31 +1,34 @@
-from mongoengine import Document, EmbeddedDocument, IntField, StringField, DateField, ListField, EmbeddedDocumentField
+from mongoengine import Document, EmbeddedDocument, IntField, StringField, \
+                        DateTimeField, ListField, EmbeddedDocumentField
 
 
 class Job(EmbeddedDocument):
     fileName = StringField(required=True)
-    storagePath = StringField(required=True)
     description = StringField(required=True)
     user = StringField(required=True)
-    timeSubmitted = DateField(required=True)
-    timeRan = DateField()
+    timeSubmitted = DateTimeField(required=True)
+    timeRan = DateTimeField()
     status = IntField()
+    meta = {"db_alias": "agent_db"}
 
 
 class Agent(Document):
-    _id = StringField(required=True)
     hostname = StringField(required=True)
     jobsQueue = ListField(EmbeddedDocumentField(Job))
     jobsHistory = ListField(EmbeddedDocumentField(Job))
+    meta = {"db_alias": "agent_db"}
 
 
 class Library(Document):
     jobs = ListField(EmbeddedDocumentField(Job))
+    meta = {"db_alias": "agent_db"}
 
 
 class Session(EmbeddedDocument):
     username = StringField(required=True)
     authToken = StringField(required=True)
-    expires = DateField(required=True)
+    expires = DateTimeField(required=True)
+    meta = {"db_alias": "admin_db"}
 
 
 class User(Document):
@@ -33,3 +36,4 @@ class User(Document):
     username = StringField(required=True)
     passwordHash = StringField(required=True)
     sessions = ListField(EmbeddedDocumentField(Session))
+    meta = {"db_alias": "admin_db"}
