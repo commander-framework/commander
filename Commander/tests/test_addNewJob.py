d282a70f13833198ef3c99aa5523a7fae5a7e1a2
C.J. May <doctormay6@gmail.com>
2021-11-08 16:32:03 -0600

    added test for expired session

--- Commander/tests/test_addNewJob.py
+++ Commander/tests/test_addNewJob.py
@@ -42,3 +42,23 @@ def testNewJob(client, sample_Job, sample_valid_Session, sample_User):
     # clean up database for next test
     agentDB.drop_database("agents")
     adminDB.drop_database("admins")
+
+
+def testExpiredSessionNewJob(client, sample_Job, sample_expired_Session, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user["sessions"].append(sample_expired_Session)
+    user.save()
+    # add new job to the library
+    data = {"job": sample_Job.to_json(),
+            "file": (io.BytesIO(b"test content"), "testfile")}
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data",
+                                    "Auth-Token": sample_expired_Session["authToken"],
+                                    "Username": sample_expired_Session["username"]},
+                           data=data)
+    assert response.status_code == 403
+    assert response.json["error"] == "invalid auth token or token expired"
+    # clean up database for next test
+    agentDB.drop_database("agents")
+    adminDB.drop_database("admins")
