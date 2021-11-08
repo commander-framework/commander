e4d4d128c68d2dc6f5864415ccb11342899b62f0
C.J. May <doctormay6@gmail.com>
2021-11-08 17:33:04 -0600

    wrote tests for all addNewJob errors

--- Commander/tests/test_addNewJob.py
+++ Commander/tests/test_addNewJob.py
@@ -55,3 +55,84 @@ def testExpiredSessionNewJob(client, sample_Job, sample_expired_Session, sample_
                            data=data)
     assert response.status_code == 403
     assert response.json["error"] == "invalid auth token or token expired"
+
+
+def testMissingFileNewJob(client, sample_Job, sample_valid_Session, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user["sessions"].append(sample_valid_Session)
+    user.save()
+    # add new job to the library
+    data = {"job": sample_Job.to_json(),
+            "file": "testfile"}
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data",
+                                    "Auth-Token": sample_valid_Session["authToken"],
+                                    "Username": sample_valid_Session["username"]},
+                           data=data)
+    assert response.status_code == 400
+    assert response.json["error"] == "file not uploaded with request"
+
+
+def testMissingJobFieldNewJob(client, sample_Job, sample_valid_Session, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user["sessions"].append(sample_valid_Session)
+    user.save()
+    job = json.loads(sample_Job.to_json())
+    job.pop("executor")
+    job.pop("filename")
+    job.pop("description")
+    job.pop("os")
+    job.pop("user")
+    job.pop("timeCreated")
+    # add new job to the library
+    data = {"job": json.dumps(job),
+            "file": (io.BytesIO(b"test content"), "testfile")}
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data",
+                                    "Auth-Token": sample_valid_Session["authToken"],
+                                    "Username": sample_valid_Session["username"]},
+                           data=data)
+    assert response.status_code == 400
+    assert response.json["error"] == "the job in the request is missing the following fields: ['executor', 'filename', 'description', 'os', 'user', 'timeCreated']"
+
+
+def testDuplicateNewJob(client, sample_Job, sample_valid_Session, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user["sessions"].append(sample_valid_Session)
+    user.save()
+    # add new job to the library
+    data = {"job": sample_Job.to_json(),
+            "file": (io.BytesIO(b"test content"), "testfile")}
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data",
+                                    "Auth-Token": sample_valid_Session["authToken"],
+                                    "Username": sample_valid_Session["username"]},
+                           data=data)
+    assert response.status_code == 200
+    assert response.json["success"] == "successfully added new executable to the commander library"
+    # add same job document with a new file to test error
+    data = {"job": sample_Job.to_json(),
+            "file": (io.BytesIO(b"test content 2"), "testfile2")}
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data",
+                                    "Auth-Token": sample_valid_Session["authToken"],
+                                    "Username": sample_valid_Session["username"]},
+                           data=data)
+    assert response.status_code == 400
+    assert response.json["error"] == "file name already exists in the library"
+
+
+def testMissingFieldsNewJob(client, sample_valid_Session, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user["sessions"].append(sample_valid_Session)
+    user.save()
+    # add new job to the library
+    response = client.post("/admin/library",
+                           headers={"Content-Type": "multipart/form-data"},
+                           data=json.dumps({}))
+    assert response.status_code == 400
+    assert response.json["error"] == "request is missing one or more of the following parameters: headers=['Auth-Token', 'Username'], data=['job', 'file']"
