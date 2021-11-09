6806918d0f0b87e0c25289dd7f605ff5594804c6
C.J. May <doctormay6@gmail.com>
2021-11-08 21:53:46 -0600

    wrote tests for login

---
+++ Commander/tests/test_login.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import json
+from utils import timestampToDatetime
+
+
+def testLogin(client, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user.save()
+    # send login credentials to get a session token
+    response = client.post("/admin/login",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"username": sample_User["username"],
+                                            "password": "samplePass"}))
+    assert response.status_code == 200
+    assert "token" in response.json
+    token = response.json["token"]
+    assert "expires" in response.json
+    expiration = timestampToDatetime(response.json["expires"])
+    assert expiration > datetime.now()
+    # make sure a valid session was saved to the db
+    response = client.get("/admin/authenticate",
+                          headers={"Content-Type": "application/json",
+                                   "Auth-Token": token,
+                                   "Username": sample_User["username"]},
+                          data=json.dumps({}))
+    assert response.status_code == 200
+    assert response.json["success"] == "authentication token is valid"
+
+
+def testBadUsernameLogin(client, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user.save()
+    # send login credentials to get a session token
+    response = client.post("/admin/login",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"username": "not_a_valid_user",
+                                            "password": "samplePass"}))
+    assert response.status_code == 401
+    assert response.json["error"] == "username not found"
+
+
+def testBadPasswordLogin(client, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user.save()
+    # send login credentials to get a session token
+    response = client.post("/admin/login",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"username": sample_User["username"],
+                                            "password": "wrong_password"}))
+    assert response.status_code == 401
+    assert response.json["error"] == "password does not match"
+
+
+def testMissingFieldsLogin(client, sample_User):
+    # prepare mongomock with relevant sample documents
+    user = sample_User
+    user.save()
+    # send login credentials to get a session token
+    response = client.post("/admin/login",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({}))
+    assert response.status_code == 400
+    assert response.json["error"] == "request is missing the following parameters: data=['username', 'password']"
