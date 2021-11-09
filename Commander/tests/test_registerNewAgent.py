497dc6da424e2bc29769b0183bf2a723f532b3a3
C.J. May <doctormay6@gmail.com>
2021-11-09 15:20:24 -0600

    wrote tests for registerNewAgent

---
+++ Commander/tests/test_registerNewAgent.py
@@ -0,0 +1,59 @@
+import json
+
+
+def testRegisterAgent(client, sample_RegistrationKey, sample_Agent):
+    # prepare mongomock with relevant sample documents
+    regKey = sample_RegistrationKey
+    regKey.save()
+    # send job results to the api server
+    response = client.post("/agent/register",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"registrationKey": sample_RegistrationKey["regKey"],
+                                            "hostname": sample_Agent["hostname"],
+                                            "os": sample_Agent["os"]}))
+    assert response.status_code == 200
+    assert "agentID" in response.json
+    # check in with new agent to make sure it was saved to the db
+    agentID = response.json["agentID"]
+    response = client.get("/agent/jobs",
+                          headers={"Content-Type": "application/json",
+                                   "Agent-ID": agentID})
+    assert response.status_code == 204
+
+
+def testNoRegKeyRegisterAgent(client, sample_RegistrationKey, sample_Agent):
+    # intentionally not saving reg key to db
+    # send job results to the api server
+    response = client.post("/agent/register",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"registrationKey": sample_RegistrationKey["regKey"],
+                                            "hostname": sample_Agent["hostname"],
+                                            "os": sample_Agent["os"]}))
+    assert response.status_code == 500
+    assert response.json["error"] == "no registration key has been generated yet"
+
+
+def testBadKeyRegisterAgent(client, sample_RegistrationKey, sample_Agent):
+    # prepare mongomock with relevant sample documents
+    regKey = sample_RegistrationKey
+    regKey.save()
+    # send job results to the api server
+    response = client.post("/agent/register",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({"registrationKey": "not_a_valid_regkey",
+                                            "hostname": sample_Agent["hostname"],
+                                            "os": sample_Agent["os"]}))
+    assert response.status_code == 401
+    assert response.json["error"] == "invalild registration key"
+
+
+def testMissingFieldsRegisterAgent(client, sample_RegistrationKey):
+    # prepare mongomock with relevant sample documents
+    regKey = sample_RegistrationKey
+    regKey.save()
+    # send job results to the api server
+    response = client.post("/agent/register",
+                           headers={"Content-Type": "application/json"},
+                           data=json.dumps({}))
+    assert response.status_code == 400
+    assert response.json["error"] == "request is missing the following parameters: data=['registrationKey', 'hostname', 'os']"
