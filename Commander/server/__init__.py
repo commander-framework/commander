71532979f857390dd52378741f630c17c391270c
C.J. May <doctormay6@gmail.com>
2022-03-17 09:51:05 -0500

    added flask-sock package

--- Commander/server/__init__.py
+++ Commander/server/__init__.py
@@ -1,9 +1,11 @@
 from config import Config
 from flask import Flask
+from flask_sock import Sock
 from mongoengine import connect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+sock = Sock(app)
 
 agentDB = connect(db="agents",
                   alias="agent_db",
