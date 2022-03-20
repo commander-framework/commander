b49803c57ceea31bb3586b75a42336aeea6e87ca
C.J. May <doctormay6@gmail.com>
2022-03-19 19:10:18 -0500

    added server ping interval

--- Commander/config.py
+++ Commander/config.py
@@ -2,11 +2,12 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
+    CA_HOSTNAME = os.environ.get("CA_HOSTNAME") or "CAPy.local"
     DB_USER = os.environ.get("DB_USER") or None
     DB_PASS = os.environ.get("DB_PASS") or None
     DB_URI = os.environ.get("DB_URI") or "mongomock://localhost"
-    CA_HOSTNAME = os.environ.get("CA_HOSTNAME") or "CAPy.local"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
+    SOCK_SERVER_OPTIONS = {"ping_interval": 25}
     UPLOADS_DIR = os.environ.get("UPLOADS_DIR") or "/opt/commander/library/"
     if UPLOADS_DIR[-1] != "/" and UPLOADS_DIR[-1] != "\\":
         UPLOADS_DIR += os.path.sep
