1b498691440d354c3766e3039731d46c68e432b5
C.J. May <doctormay6@gmail.com>
2022-05-05 13:22:14 -0500

    added redis initialization

--- Commander/config.py
+++ Commander/config.py
@@ -9,6 +9,7 @@ class Config(object):
     DB_URI = os.environ.get("DB_URI") or "mongomock://localhost"
     LOG_LEVEL = int(os.environ.get("LOG_LEVEL") or 4)
     JWT_SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
+    REDIS_PASS = os.environ.get("REDIS_PASS") or None
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
     SOCK_SERVER_OPTIONS = {"ping_interval": 25}
     UPLOADS_DIR = os.environ.get("UPLOADS_DIR") or "/opt/commander/library"
