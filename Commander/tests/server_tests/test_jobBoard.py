34e9c887d5a4b426f30142528ed2ff58ac2af9c1
C.J. May <doctormay6@gmail.com>
2022-05-15 12:14:26 -0500

    added async test

--- Commander/tests/server_tests/test_jobBoard.py
+++ Commander/tests/server_tests/test_jobBoard.py
@@ -1,48 +1,51 @@
+from copy import deepcopy
+import gevent
 import json
-from multiprocessing import Process
-import time
+from server import jobsCache
 
-def test_assignJob(jobBoard, sample_Job, sample_Agent):
+def test_assignJob(sample_Job, sample_Agent):
     # prepare mongomock with relevant sample documents
     agent = sample_Agent
     agent.save()
     # assign job to agent
-    jobsCache = jobBoard
     jobsCache.assignJob(sample_Job, agentID=agent["agentID"])
     agent.reload()
     assert json.loads(sample_Job.to_json()) in jobsCache.agentCheckin(agent["agentID"])
     assert sample_Job in agent["jobsQueue"]
 
 
-def test_noIdAssignJob(jobBoard, sample_Job):
-    jobsCache = jobBoard
+def test_noIdAssignJob(sample_Job):
     try:
         jobsCache.assignJob(sample_Job)
     except ValueError as e:
         assert "must specify either agentID or groupID when assigning a job" == str(e)
 
 
-# def test_async_assignJob(jobBoard, sample_Job, sample_Agent):
-#     # prepare mongomock with relevant sample documents
-#     agent = sample_Agent
-#     agent.save()
-#     # assign multiple jobs to agent in parallel
-#     jobsCache = jobBoard
-#     p1 = Process(target=jobsCache.assignJob, args=(sample_Job, agent["agentID"]))
-#     p2 = Process(target=jobsCache.assignJob, args=(sample_Job, agent["agentID"]))
-#     p3 = Process(target=jobsCache.assignJob, args=(sample_Job, agent["agentID"]))
-#     p4 = Process(target=jobsCache.assignJob, args=(sample_Job, agent["agentID"]))
-#     p1.start()
-#     p2.start()
-#     p3.start()
-#     p4.start()
-#     p1.join()
-#     p2.join()
-#     p3.join()
-#     p4.join()
-#     time.sleep(3)
-#     agent.reload()
-#     assert json.loads(sample_Job.to_json()) in jobsCache.agentCheckin(agent["agentID"])
-#     assert len(jobsCache.agentCheckin(agent["agentID"])) == 4
-#     assert sample_Job in agent["jobsQueue"]
-#     assert len(agent["jobsQueue"]) == 4
+def test_async_assignJob(sample_Job, sample_Agent):
+    # prepare mongomock and relevant sample documents
+    agent = sample_Agent
+    agent.save()
+    sample_Job1 = deepcopy(sample_Job)
+    sample_Job1.filename = "proc1"
+    sample_Job2 = deepcopy(sample_Job)
+    sample_Job2.filename = "proc2"
+    sample_Job3 = deepcopy(sample_Job)
+    sample_Job3.filename = "proc3"
+    sample_Job4 = deepcopy(sample_Job)
+    sample_Job4.filename = "proc4"
+    # assign multiple jobs to agent in parallel
+    gevent.joinall([gevent.spawn(jobsCache.assignJob, sample_Job1, agent["agentID"]),
+                    gevent.spawn(jobsCache.assignJob, sample_Job2, agent["agentID"]),
+                    gevent.spawn(jobsCache.assignJob, sample_Job3, agent["agentID"]),
+                    gevent.spawn(jobsCache.assignJob, sample_Job4, agent["agentID"])])
+    agent.reload()
+    assert json.loads(sample_Job1.to_json()) in jobsCache.agentCheckin(agent["agentID"])
+    assert json.loads(sample_Job2.to_json()) in jobsCache.agentCheckin(agent["agentID"])
+    assert json.loads(sample_Job3.to_json()) in jobsCache.agentCheckin(agent["agentID"])
+    assert json.loads(sample_Job4.to_json()) in jobsCache.agentCheckin(agent["agentID"])
+    assert len(jobsCache.agentCheckin(agent["agentID"])) == 4
+    assert sample_Job1 in agent["jobsQueue"]
+    assert sample_Job2 in agent["jobsQueue"]
+    assert sample_Job3 in agent["jobsQueue"]
+    assert sample_Job4 in agent["jobsQueue"]
+    assert len(agent["jobsQueue"]) == 4
