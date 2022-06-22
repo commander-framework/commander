19f6237a16df5d24ee0dd055c84dec13a7a63b4c
C.J. May <doctormay6@gmail.com>
2022-06-22 14:33:17 -0500

    updated order

--- tests/integration/test_jobWorkflow.py
+++ tests/integration/test_jobWorkflow.py
@@ -1,26 +1,26 @@
 import pytest
 
 
-@pytest.mark.order(3)
+@pytest.mark.order(4)
 def test_createJob():
     pass
 
 
-@pytest.mark.order(4)
+@pytest.mark.order(5)
 def test_agentCheckinNoJob():
     pass
 
 
-@pytest.mark.order(5)
+@pytest.mark.order(6)
 def test_assignJob():
     pass
 
 
-@pytest.mark.order(6)
+@pytest.mark.order(7)
 def test_fetchAndExecuteJob():
     pass
 
 
-@pytest.mark.order(7)
+@pytest.mark.order(8)
 def test_getJobHistory():
     pass
