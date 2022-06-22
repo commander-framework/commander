eae2c3799f1ff17eca849475e9062983850798f6
C.J. May <doctormay6@gmail.com>
2022-06-22 18:13:01 -0500

    fixed typo

--- Commander/run.sh
+++ Commander/run.sh
@@ -2,9 +2,9 @@
 
 # start flask service
 gunicorn -b 0.0.0.0:5000 \
-         --workers ${WORKERS} \
-		 --worker_connections ${WORKER_CONNECTIONS} \
-		 --worker_class gevent \
+         --workers $WORKERS \
+		 --worker-connections $WORKER_CONNECTIONS \
+		 --worker-class gevent \
 		 --worker-tmp-dir /dev/shm \
 		 --preload \
 		 server:app
