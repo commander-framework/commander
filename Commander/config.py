889f7af951085e926ff60f1cf433ac1eef7cdfc0
C.J. May <doctormay6@gmail.com>
2022-07-14 11:47:40 -0500

    uploads dir not specified at runtime

--- Commander/config.py
+++ Commander/config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     ADMIN_HASH = os.environ.get("ADMIN_HASH") or ""
     APP_NAME = os.environ.get("APP_NAME") or "Commander"
-    CA_HOSTNAME = os.environ.get("CA_HOSTNAME") or "CAPy.local"
+    CA_HOSTNAME = os.environ.get("CA_HOSTNAME") or "capy"
     DB_USER = os.environ.get("DB_USER") or None
     DB_PASS = os.environ.get("DB_PASS") or None
     DB_URI = os.environ.get("DB_URI") or "mongomock://localhost"
@@ -14,6 +14,4 @@ class Config(object):
     REDIS_URI = os.environ.get("REDIS_URI") or "redis://localhost:6379"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
     SOCK_SERVER_OPTIONS = {"ping_interval": 25}
-    UPLOADS_DIR = os.environ.get("UPLOADS_DIR") or "/opt/commander/library"
-    if UPLOADS_DIR[-1] != "/" and UPLOADS_DIR[-1] != "\\":
-        UPLOADS_DIR += os.path.sep
+    UPLOADS_DIR = f"/opt/Commander/library/"
