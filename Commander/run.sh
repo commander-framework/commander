62ba514bdb4c4e28a5945e0e5bb2c181431b95bc
C.J. May <doctormay6@gmail.com>
2022-07-14 14:30:35 -0500

    fixed environment variable passthrough

--- Commander/run.sh
+++ Commander/run.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
-
-# add virtualenv to PATH
-export VIRTUAL_ENV=/home/abc/venv
-export PATH="/home/abc/venv/bin:$PATH"
+#!/usr/bin/with-contenv bash
 
 # start flask service
 gunicorn -b 0.0.0.0:5000 \
